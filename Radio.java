c720d52e7d974915e4a3d1406af4bda501cf655e
Alex Mora <162t0308@itsm.edu.mx>
2017-03-09 22:49:23 -0600

    Create Radio.java

---
+++ Radio.java
@@ -0,0 +1,26 @@
+public class Radio {
+private double radio;
+private String color;
+
+public Radio(){
+	radio=1.0;
+	color= "rojo";
+
+}
+public Radio(double r, String c){
+	radio = r;
+	color= c;
+   
+		
+}
+public double getRadio(){
+	return radio;
+}
+public String getColor(){
+	return color;
+	
+}
+public double getArea(){
+return radio*radio*Math.PI;	
+}
+}
