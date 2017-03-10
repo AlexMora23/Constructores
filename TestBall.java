f77b13bd466180a70e11d0e58df01e1c7a0d2147
Alex Mora <162t0308@itsm.edu.mx>
2017-03-09 22:48:49 -0600

    Create TestBall.java

---
+++ TestBall.java
@@ -0,0 +1,13 @@
+public class TestBall {
+public static void main(String [] args){
+	Ball b1 = new Ball();
+	System.out.println(b1.mensaje()+" x= "+b1.getX()+", y= "+b1.getY());
+	
+	Ball b2 = new Ball();
+	System.out.println(b2.mensaje()+" x= "+b2.getX()+", y= "+b2.getY());
+	
+	Ball b3 = new Ball();
+	System.out.println(b3.mensaje()+" x= "+b3.getX()+", y= "+b3.getY());
+    
+}
+}
